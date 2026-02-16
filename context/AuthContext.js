6ade36f9a33c3bf39213fefe8a9388026668c9d9
Aditya Das <adityadasofficial31@gmail.com>
2026-02-16 13:11:57 +0530

    logout button

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -11,7 +11,7 @@ export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(null);
 
   // 🌍 API Base URL
-  const API = "http://192.168.0.101:5000/api/v1/user";
+  const API = "http://192.168.20.41:5000/api/v1/user";
   // const API = "https://whisp-backend-api.onrender.com/api/v1/user";
 
   // 📌 Load token on app start
