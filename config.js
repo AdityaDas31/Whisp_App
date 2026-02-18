ebb52bbcc1215cb72985baf59e22976d36d32f07
Akashdelegend <akash.de2000@gmail.com>
2026-02-18 10:33:52 +0530

    story screen

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
-const API_BASE_URL =  'http://192.168.20.41:5000/api/v1'
-const SOCKET_URL = 'http://192.168.20.41:5000'
-// const API_BASE_URL =  'https://whisp-backend-api.onrender.com/api/v1'
-// const SOCKET_URL = 'https://whisp-backend-api.onrender.com'
+// const API_BASE_URL =  'http://192.168.20.41:5000/api/v1'
+// const SOCKET_URL = 'http://192.168.20.41:5000'
+const API_BASE_URL =  'https://whisp-backend-api.onrender.com/api/v1'
+const SOCKET_URL = 'https://whisp-backend-api.onrender.com'
 
 export { API_BASE_URL, SOCKET_URL }
+
